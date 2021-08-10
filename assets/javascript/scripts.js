da919e511be765cf2471917d8e7c1d7735d354cf
LuksFay <lukspicco@hotmail.com>
2021-08-10 15:07:11 -0300

    first commit

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,15 @@
+let header = document.getElementById('header_id');
+let history = document.querySelector('.history');
+let div = document.querySelector('#banner_id');
+window.onscroll = function(ev) {
+    if(window.scrollY === 0){
+        
+        header.classList.remove('scroll');
+        history.classList.remove('changeColor');
+        div.style='height:700px';
+    }else{
+        header.classList.add('scroll');
+        history.classList.add('changeColor');
+        div.style='height:760px';
+    }
+}
