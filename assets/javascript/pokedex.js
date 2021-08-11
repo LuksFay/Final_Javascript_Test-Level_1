2f0521380e0c32e58899634600496c1077bcdf9a
LuksFay <lukspicco@hotmail.com>
2021-08-11 17:06:38 -0300

    frustrado

--- assets/javascript/pokedex.js
+++ assets/javascript/pokedex.js
@@ -1,19 +1,35 @@
-// https://pokeapi.co/api/v2/pokemon
+const espacioParaCadaPokemon = document.querySelector('#pokemon_section');
 
+let arrayDePokemonEnPantalla = []
 
-function llamarPokemon() {
-    for(let pokemonId = 1; pokemonId < 152 ; pokemonId++ ){
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-         .then(res => res.json())
-         .then(objetosPokemon => {
-             pokeInfo()
-             console.log('estos son objetos pokemon', objetosPokemon )
-         })
+let cargando = document.createElement('p')
+cargando.textContent = 'CARGANDO...'
+espacioParaCadaPokemon.appendChild(cargando) 
+
+async function llamarPokemon() {
+    for(let i = 1 ; i < 152 ; i++ ){
+        const urlApi = `https://pokeapi.co/api/v2/pokemon/${i}`
+        const respuestaApi = await fetch(urlApi)
+        const unPokemon = await respuestaApi.json()
+            console.log('loading')
+            arrayDePokemonEnPantalla.push({
+                image: unPokemon.sprites.front_default,
+                id: unPokemon.id,
+                name: unPokemon.name,
+                type: unPokemon.types[0].type.name
+            })
     }
-};
+    console.log('load complete')
+    cargando.style.display = 'none'
+    for(let i = 0 ; i < 151 ; i++ ){
+        let a = arrayDePokemonEnPantalla;
+        console.log('los 150',a[i])
+        a.forEach( a => {
+            document.write
+        });
+    }   
+        
+    };
+    
 
-function pokeInfo(info){
-        console.log('esto es pokeinfo', info)
-}
-
-llamarPokemon()
+    llamarPokemon()
